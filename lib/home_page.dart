0b683ef5173aae4333add88f2c518a2c55bb31fd
sandeepgupta7620 <sandeepg9321@gmail.com>
2023-07-02 15:29:11 +0530

    Day 2 completed

---
+++ lib/home_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    const int A = 30;
+    const double B = 25.9;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog App"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Hello bacho $A and $B"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_first_app/home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,13 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text("My Name is Sandeep Gupta"),
-          ),
-        ),
-      ),
+      home: HomePage(),
     );
   }
 }
